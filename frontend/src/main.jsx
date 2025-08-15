5f2fb84c25f9554e49397eeb232b160b30189876
SurajPatel2024 <vikaskumarpatel122@gmail.com>
2025-08-15 16:58:32 +0530

    main and app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import { HashRouter } from "react-router-dom"; // <-- HashRouter instead of BrowserRouter
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
