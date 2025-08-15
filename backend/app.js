2e3aff739073577b88cc18666d87b2ad42e53307
SurajPatel2024 <vikaskumarpatel122@gmail.com>
2025-08-15 13:49:15 +0530

    page refreshing

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,10 @@ mongoose.connect(DB_URI)
 
 
 // Serve React frontend
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 
