75468ba5a2ef11a243945744c1747b3805d9488b
SurajPatel2024 <vikaskumarpatel122@gmail.com>
2025-08-15 09:41:23 +0530

    changed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: import.meta.env.VITE_API_URL, // Render backend URL
+  baseURL: "https://electronic-dukaan.onrender.com", // Render backend URL
   withCredentials: true, // send cookies
 });
 
